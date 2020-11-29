d09ca20751706ee470df482cdc942ff6ab1ecb0f
hannah627 <hannah06@uw.edu>
2020-11-28 22:43:18 -0800

    some more work on chart 1 dealing with the datasets

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,5 +1,10 @@
 chart_1 <- function (netflix_data, disney_data, amazon_data){
-  
+  netflix_genres <- netflix_data %>%
+    filter(type == "TV Show") #%>%
+    #mutate(genre = )
+  disney_genres <- disney_data %>%
+    filter(type == "series") %>%
+    group_by(genre)
 }
 
 
