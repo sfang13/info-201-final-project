40af63fafe6598d124dc908330969a6533d384dc
hannah627 <hannah06@uw.edu>
2020-11-29 15:07:34 -0800

    wrote intro paragraph and did some work on chart 1

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,10 +1,33 @@
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
 chart_1 <- function (netflix_data, disney_data, amazon_data){
   netflix_genres <- netflix_data %>%
-    filter(type == "TV Show") #%>%
-    #mutate(genre = )
+    str_replace_all("Docuseries,", "Docuseries") %>%
+    filter(type == "TV Show") %>%
+    mutate(genre = word(listed_in, 1)) %>%
+    count(genre) %>%
+    mutate(streaming_service = "Netflix")
+  
+  #netflix_genres["Docuseries", ] <- sum(netflix_genres["Docuseries,", n], netflix_genres["Docuseries", n])
+  #netflix_genres$genre <- sum(netflix_genres$genre[5], netflix_genres$genre[6])
+  #str_replace_all(c(netflix_genres$genre, "Docuseries,", "Docuseries"))
+  
+  #netflix_genres$"Docuseries" = netflix_genres$"Docuseries" + netflix_genres$"Docuseries,"
+  #Docuseries = sum(Docuseries + Docuseries,)
+  
+    
   disney_genres <- disney_data %>%
     filter(type == "series") %>%
-    group_by(genre)
+    mutate(genre = word(genre, 1)) %>%
+    count(genre) %>%
+    mutate(streaming_service = "Disney+")
+  
+  amazon_genres <- amazon_data %>%
+    mutate(genre = word(Genre, 1)) %>%
+    count(genre) %>%
+    mutate(streaming_service = "Amazon Prime Video")
 }
 
 
