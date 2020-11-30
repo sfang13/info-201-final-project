0d4fa63417daaa75340e3d2ebefd8cd975b8c180
RJ002-Chris <rj002@uw.edu>
2020-12-01 02:35:45 +0700

    edited lintr errors

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -7,11 +7,14 @@ library("tidyverse")
 
 # read the data file
 
-amazon_df <- read.csv("data/amazon_prime_tv_shows.csv", stringsAsFactors = FALSE)
-disney_df <- read.csv("data/disney_plus_shows.csv", stringsAsFactors = FALSE)
-netflix_df <- read.csv("data/netflix_movies_and_tv.csv", stringsAsFactors = FALSE)
+amazon_df <- read.csv("data/amazon_prime_tv_shows.csv"
+                      , stringsAsFactors = FALSE)
+disney_df <- read.csv("data/disney_plus_shows.csv"
+                      , stringsAsFactors = FALSE)
+netflix_df <- read.csv("data/netflix_movies_and_tv.csv"
+                       , stringsAsFactors = FALSE)
 
-amazon_df <- amazon_df %>% 
+amazon_df <- amazon_df %>%
   rename(
     imdb_rating = IMDb.rating,
     genre = Genre,
@@ -19,31 +22,34 @@ amazon_df <- amazon_df %>%
     year = Year.of.release
   )
 
-# The group calculation was done because each of the data sets
-# we are using have different shows and movies from different companies.
-# The data can be applied to show the total number of movies/shows in the set,
-# the highest and lowest ratings, the most common language used, most common rating
-# and the most common genre between all the shows/movies.
+# The group calculation was done because each of the
+# data sets we are using have different shows and movies
+# from different companies. The data can be applied to show
+# the total number of movies/shows in the set, the highest
+# and lowest ratings, the most common language used, most
+# common rating and the most common genre between all the
+# shows and movies.
 
 summary_table <- function(dataset) {
   get_summary <- dataset %>%
-  group_by(year) %>%
-  drop_na() %>%
-  na.omit() %>%
-  filter(!(plot == "N/A" | imdb_rating == "N/A" | imdb_votes == "N/A" | awards == "N/A"
-           | country == "N/A" | language == "N/A" | actors == "N/A" | writer == "N/A"
-           | director == "N/A" | genre == "N/A" | runtime == "N/A" | released_at == "N/A"
-           | rated == "N/A" | metascore == "N/A" | imdb_id == "")
-  ) %>%
-  summarise(
-    total_movies = n(),
-    highest_rating = max(imdb_rating, na.rm = TRUE),
-    lowest_rating = min(imdb_rating, na.rm = TRUE),
-    most_common_language = max(language, na.rm = TRUE),
-    most_common_rating = max(rated, na.rm = TRUE),
-    most_common_genre = max(genre, na.rm = TRUE),
-  ) %>%
-  arrange(year)
+    group_by(year) %>%
+    drop_na() %>%
+    na.omit() %>%
+    filter(!(plot == "N/A" | imdb_rating == "N/A"
+             | imdb_votes == "N/A" | awards == "N/A"
+    | country == "N/A" | language == "N/A" | actors == "N/A"
+    | writer == "N/A" | director == "N/A" | genre == "N/A"
+    | runtime == "N/A" | released_at == "N/A"
+    | rated == "N/A" | metascore == "N/A" | imdb_id == "")) %>%
+    summarise(
+      total_movies = n(),
+      highest_rating = max(imdb_rating, na.rm = TRUE),
+      lowest_rating = min(imdb_rating, na.rm = TRUE),
+      most_common_language = max(language, na.rm = TRUE),
+      most_common_rating = max(rated, na.rm = TRUE),
+      most_common_genre = max(genre, na.rm = TRUE),
+    ) %>%
+    arrange(year)
   return(get_summary)
 }
 
@@ -58,7 +64,9 @@ summary_table_format <- formattable(summary_table,
   ))
 )
 
-# I included the table to make summarize the big data frames and make the information
-# easier to visualize when looking at the summarized table.
-# The table shows the total number of movies in that year, the highest and lowest ratings
-# in that year, the most common genre, and the most  common language used in all of the movies.
+# I included the table to make summarize the big data frames
+# and make the information easier to visualize when looking
+# at the summarized table. The table shows the total number
+# of movies in that year, the highest and lowest ratings
+# in that year, the most common genre, and the most common
+# language used in all of the movies.
