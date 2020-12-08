a056bcdd866e5206cc5250421ddc76c7bcbad23d
hannah627 <hannah06@uw.edu>
2020-12-08 00:54:31 -0800

    created and did preliminary work on chart 1 (got chart to render); also loaded in datasets and chart files

--- app_server.R
+++ app_server.R
@@ -3,6 +3,27 @@ library("ggplot2")
 library("plotly")
 library("shiny")
 
+#read in datasets
+amazon_data <- read.csv("data/amazon_prime_tv_shows.csv",
+                        stringsAsFactors = FALSE
+)
+disney_data <- read.csv("data/disney_plus_shows.csv",
+                        stringsAsFactors = FALSE
+)
+netflix_data <- read.csv("data/netflix_movies_and_tv.csv",
+                         stringsAsFactors = FALSE
+)
+netflix_2_data <- read.csv("data/netflix_movies_and_tv_imdb_ratings.csv",
+                           stringsAsFactors = FALSE
+)
+
+#read in chart files
+source("scripts/chart_1.R")
+source("scripts/chart_2.R")
+source("scripts/chart_3.R")
+
 server <- function(input, output) {
-  
+  output$chart1 <- renderPlotly(
+    return(chart_1(netflix_data, disney_data, amazon_data))
+  )
 }
--- app_ui.R
+++ app_ui.R
@@ -1,14 +1,45 @@
 library("shiny")
 library("plotly")
 
+
 page_one <- tabPanel (
   "Page one",
   sidebarLayout(),
-    mainPanel()
+  mainPanel()
+)
+
+
+#hannah's chart page --------
+
+#creates side panel with widgets for chart 1 page
+chart1_side_panel <- sidebarPanel(
+  
+)
+
+#creates main panel with actual bar graph for chart 1 page
+chart1_main_panel <- mainPanel(
+  h1("What genres are most popular on each streaming platform? Do these align
+     with other streaming platforms?"),
+  plotlyOutput("chart1")
+)
+
+#creates the actual chart 1 page
+chart_page_one <- tabPanel(
+  "Popularity of Genres for TV Shows",
+  sidebarLayout(
+    chart1_side_panel,
+    chart1_main_panel
   )
+)
+
+# ---------------------------
 
 
+
+
+#ui for creating shinyApp
 ui <- navbarPage(
   "title",
-  page_one
+  page_one,
+  chart_page_one
 )
