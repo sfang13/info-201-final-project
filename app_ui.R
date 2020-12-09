78d34de78bfbf1c375820c8d4db419844eba4d24
RJ002-Chris <rj002@uw.edu>
2020-12-10 00:44:30 +0700

    added images

--- app_ui.R
+++ app_ui.R
@@ -4,24 +4,25 @@ library("shinythemes")
 
 # Chris' overview page --------
 overview_sidepanel <- sidebarPanel(
-  tags$p(strong("Page one: Genres"), "What genres are most popular on each
+  tags$p(strong("Page one: Genre Popularity"), "What genres are most popular on each
          streaming platform?"),
-  tags$p(strong("Page 2: Age Trends"), "What trends have emerged in producing
+  tags$p(strong("Page 2: Age Demographics"), "What trends have emerged in producing
          content for each age demographic?"),
   tags$p(strong("Page 3: ---"), "---"),
-  tags$ul(
-    tags$li(helpText("Current best on-demand streaming services:",
-                     tags$a(href = "https://images.moneycontrol.com
-                            /static-mcnews/2020/04/Netflix_Disney_Prime_Video-770x433
-                            .jpg?impolicy=website&width=770&height=431",
-                            "Streaming Platforms")))
-  )
+  tags$h4("Current best selling TV streaming platforms:"),
+  tags$ol(
+    tags$li("HBO Now"),
+    tags$li("Youtube TV"),
+    tags$li("Netflix"),
+    tags$li("Sling TV"),
+    tags$li("Amazon Prime"),
+  ),
+  tags$img(src = "our_services2.jpg", height = "85%", width = "85%"),
 )
 
 overview_mainpanel <- mainPanel(
   tags$h1("Overview"),
-  br(),
-  tags$img(src = "data/streaming_services", height = "100%", width = "100%"),
+  tags$img(src = "streaming_services.jpg", height = "60%", width = "60%"),
   tags$h2("Purpose:"),
   tags$p(
     "This project's purpose is to analyze TV shows and movies from",
@@ -43,16 +44,19 @@ overview_mainpanel <- mainPanel(
     href = "https://www.kaggle.com/nilimajauhari/amazon-prime-tv-shows"
   ),
   br(),
+  br(),
   tags$p("From this data observation, we will be able to answer
     the following questions:"),
-  tags$h5("What are the user demographics of various streaming platforms?
-     How have they changed over time?"),
-  tags$h5("What genres are most popular on each streaming platform?
-     How have the popular genres changed over time?"),
-  tags$h5("How have the duration, ratings, viewers, and genres of tv shows and movies
-     on various streaming platforms changed over the years? In 2020?"),
+  tags$ul(
+    tags$li("What are the user demographics of various streaming platforms?
+       How have they changed over time?"),
+    tags$li("What genres are most popular on each streaming platform?
+       How have the popular genres changed over time?"),
+    tags$li("How have the duration, ratings, viewers, and genres of tv shows and movies
+       on various streaming platforms changed over the years? In 2020?")
+  ),
   tags$p("We will be using our skills that we've learned throughout this quarter to
-    analyze the datasets and obtain valuable information from them."),
+      analyze and filter trought the data sets to obtain valuable information from them.")
 )
 
 overview_page <- tabPanel(
